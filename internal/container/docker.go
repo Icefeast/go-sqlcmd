b5c50164ae2620e129620662d3d7c133f405e554
Ladislav Zvada <ladislav.zvada@swissre.com>
2025-03-28 11:39:11 +0100

    list tags fix

--- internal/container/docker.go
+++ internal/container/docker.go
@@ -3,30 +3,23 @@
 
 package container
 
-import (
-	"context"
-	"github.com/docker/distribution/reference"
-	"github.com/docker/distribution/registry/client"
-	"net/http"
-)
-
 // ListTags lists all tags for a container image located at a given
 // path in the container registry. It takes the path to the image and the
 // URL of the registry as input and returns a slice of strings containing
 // the tags.
 func ListTags(path string, baseURL string) []string {
-	ctx := context.Background()
-	repo, err := reference.WithName(path)
-	checkErr(err)
-	repository, err := client.NewRepository(
-		repo,
-		baseURL,
-		http.DefaultTransport,
-	)
-	checkErr(err)
-	tagService := repository.Tags(ctx)
-	tags, err := tagService.All(ctx)
-	checkErr(err)
+	// ctx := context.Background()
+	// repo, err := reference.WithName(path)
+	// checkErr(err)
+	// repository, err := client.NewRepository(
+	// 	repo,
+	// 	baseURL,
+	// 	http.DefaultTransport,
+	// )
+	// checkErr(err)
+	// tagService := repository.Tags(ctx)
+	// tags, err := tagService.All(ctx)
+	// checkErr(err)
 
-	return tags
+	return []string{}
 }
